8d94d24ca540b644502894dfb84346965fa56f6f
kimgura <oli999@naver.com>
2019-12-30 10:08:44 +0900

    lab1 브렌치에서 출력함

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -4,5 +4,6 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("kimgura");
 		System.out.println("monkey");
+		System.out.println("lab1 브렌치에서 작업함");
 	}
 }
